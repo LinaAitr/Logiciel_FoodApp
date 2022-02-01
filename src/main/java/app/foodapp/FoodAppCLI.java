8d28ebd20ef5ada44c66aa2fa2820b6fa9036b55
a20005638 <lina.ait-razzouk@etu.univ-amu.fr>
2022-02-01 16:39:28 +0100

    debut foodappCli

--- src/main/java/app/foodapp/FoodAppCLI.java
+++ src/main/java/app/foodapp/FoodAppCLI.java
@@ -8,9 +8,19 @@ public class FoodAppCLI {
 
         System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
 
-        System.out.println("Input your command: ");
+        //System.out.println("Input your command: ");
+        System.out.println("Which recipe : 1 or 2 ? ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Sorry, I can't do anything yet ! (Read: " + scanner.nextLine() +")");
-        scanner.close();
+        if(scanner.nextInt() == 1 ){
+            System.out.println("choix 1");
+            scanner.close();
+        }
+        else if(scanner.nextInt() == 2){
+            System.out.println("choix 2");
+            scanner.close();
+        }
+        else {System.out.println("Sorry, I can't do anything yet ! (Read: " + scanner.nextLine() +")");
+            scanner.close();}
+
     }
 }
