03f197fa9038e9d20836b3e827154a9f3897c49b
sofiane <sofyanekebci@gmail.com>
2022-02-07 14:36:02 +0100

    FindByIngridients RÉUSSI

--- src/main/java/app/foodapp/model/FindByIngridients.java
+++ src/main/java/app/foodapp/model/FindByIngridients.java
@@ -1,15 +1,18 @@
 package app.foodapp.model;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class FindByIngridients {
 
         public static void main(String[] args) {
+
             JSONParser jsonP = new JSONParser();
             try {
                 JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader("fichiers json/recipeInformation.json"));
@@ -20,6 +23,12 @@ public class FindByIngridients {
                 System.out.println("sourceUrl: "+ sourceUrl);
                 System.out.println("summary: "+ summary);
 
+
+                JSONArray listOfStates = (JSONArray) jsonO.get("diets");
+                Iterator iterator = listOfStates.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
             } catch (IOException | ParseException e) {
                 e.printStackTrace();
             }
