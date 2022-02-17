52cb1cd2f4fc0c617489f14a57975347c030a9c2
f20008012 <clem.fresnel@gmail.com>
2022-02-17 22:34:03 +0100

    searchBar in progress

--- src/main/java/app/foodapp/FoodAppCLI.java
+++ src/main/java/app/foodapp/FoodAppCLI.java
@@ -1,16 +1,18 @@
 package app.foodapp;
 
 import app.foodapp.model.SearchBar;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FoodAppCLI {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("Welcome to the food app");
         System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
 
         //System.out.println("Input your command: ");
-        System.out.println("Which recipe : \n - apple, \n - vegan. ");
+        System.out.println("Which recipe : \n - apple \n - vegan \n - favorite ");
         Scanner scanner = new Scanner(System.in);
         System.out.println(SearchBar.search(scanner));
         //SearchBar.selectRecipe(scanner);
