b68315d31d5e8f6f653fb9c504e1263e6af824f3
sofiane <sofyanekebci@gmail.com>
2022-02-21 14:21:37 +0100

    essaie de L'API

--- src/main/java/app/foodapp/model/TryAPI_KeyWord.java
+++ src/main/java/app/foodapp/model/TryAPI_KeyWord.java
@@ -9,10 +9,11 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
-public class TryAPI {
-
+public class TryAPI_KeyWord {
     public static void main(String[] args) throws IOException {
 
         try {
@@ -23,7 +24,7 @@ public class TryAPI {
             pour plusieurs ",+" entre les ingredients
             */
 
-            URL url = new URL("https://api.spoonacular.com/recipes/findByIngredients?apiKey=ab7c3f5a18a04dd8903bc5fdb0be40e9&ingredients=apple");
+            URL url = new URL("https://api.spoonacular.com/recipes/complexSearch?apiKey=ab7c3f5a18a04dd8903bc5fdb0be40e9&query=apple");
 
             HttpURLConnection conn =(HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -44,19 +45,29 @@ public class TryAPI {
                 }
                 scanner.close();
 
+
                 //System.out.println(informationString);
 
                 JSONParser parse = new JSONParser();
                 JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
 
+                //***********************************************
+                Iterator iterator = dataObject.iterator();
+                while (iterator.hasNext()) {
+                    Iterator<Map.Entry> itr1 = ((Map) iterator.next()).entrySet().iterator();
+                    while (itr1.hasNext()) {
+                        Map.Entry pair = itr1.next();
+                        System.out.println(pair.getKey() + " : " + pair.getValue());
+                    }
+                }
+
+                //**************************************************
                 //System.out.println(dataObject.get(0));
                 for (int i=0; i<dataObject.size();i++){
                     JSONObject recipeData = (JSONObject) dataObject.get(i);
                     System.out.print(i+"-");
                     System.out.println(recipeData.get("title"));
                 }
-
-
             }
 
 
@@ -65,3 +76,4 @@ public class TryAPI {
         }
     }
 }
+
