4ce18f8fc64eede1260bb587e40b9e49a89a86eb
f20008012 <clem.fresnel@gmail.com>
2022-02-21 10:14:51 +0100

    searchBar in progress

--- src/main/java/app/foodapp/model/SearchBar.java
+++ src/main/java/app/foodapp/model/SearchBar.java
@@ -1,5 +1,6 @@
 package app.foodapp.model;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -18,11 +19,16 @@ public class SearchBar {
 
         if (scannerMessage.equals("apple") || scannerMessage.equals("vegan") || scannerMessage.equals("favorite")) {
             if (scannerMessage.equals("apple")) {
-                System.out.println(scannerMessage);
-                JSONObject jsonO = (JSONObject) jsonP.parse(new FileReader("fichiers json/findByIngredients.json"));
-                RecipeInformations recipeInformations = new RecipeInformations(jsonO);
-                result = recipeInformations.getTitle();
+                JSONArray arrayOfRecipes = (JSONArray) jsonP.parse(new FileReader("fichiers json/findByIngredients.json"));
+
+                for (Object recipe : arrayOfRecipes) {
+                    JSONObject recipeObj = (JSONObject) recipe;
+                    RecipeInformations newRecipe = new RecipeInformations(recipeObj);
+
+                    result += newRecipe.getTitle() + "\n";
+                }
             }
+
 
             if (scannerMessage.equals("vegan")) {
                 JSONObject jsonO = (JSONObject) jsonP.parse(new FileReader("fichiers json/recipeInformation.json"));
