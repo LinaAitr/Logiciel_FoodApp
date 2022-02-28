3a3eda9300f0f117ef3bf10a4b33178f58b131dd
f20008012 <clem.fresnel@gmail.com>
2022-02-28 14:27:35 +0100

    fx

--- src/main/java/app/foodapp/controller/FoodAppController.java
+++ src/main/java/app/foodapp/controller/FoodAppController.java
@@ -2,26 +2,51 @@ package app.foodapp.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FoodAppController implements Initializable {
 
+
+    @FXML private TextField searchBar;
+    @FXML private Button searchButton;
+    @FXML private Button favoritesButton;
+
     @FXML private Button helloWorldButton;
     @FXML private Button goodByeWorldButton;
 
     @FXML private Button logInButton;
-    @FXML private Button searchButton;
     @FXML private Button dropMenuButton;
     @FXML private Button homeButton;
+
 
     @FXML private Label label;
 
     //    @Override
-    public void initialize(URL location, ResourceBundle resourceBundle) {}
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        searchBar.setAlignment(Pos.TOP_CENTER);
+
+    }
+
+    @FXML
+    private void search(){
+        searchBar.setVisible(true);
+        label.setText(searchBar.getText());
+        searchBar.setText("");
+        //logInButton.setVisible(false);
+        goodByeWorldButton.setVisible(false);
+    }
+
+    @FXML
+    private void openFavorites(){
+
+    }
+
 
     @FXML
     private void displayHelloWorld() {
@@ -36,11 +61,19 @@ public class FoodAppController implements Initializable {
         label.setText("");
         goodByeWorldButton.setVisible(false);
         if (!helloWorldButton.isVisible())
-            helloWorldButton.setVisible(true);
+            helloWorldButton.setVisible(false);
     }
 
     @FXML
     private void logIn(){
         label.setText("your log in is done ");
+    }
+
+    @FXML
+    private void welcomeToApp() {
+        label.setText("Welcome to foodApp");
+        helloWorldButton.setVisible(false);
+        if (!goodByeWorldButton.isVisible())
+            goodByeWorldButton.setVisible(true);
     }
 }
