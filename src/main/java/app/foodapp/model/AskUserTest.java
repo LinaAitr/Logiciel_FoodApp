2fc00d4e5fdae39dad21bfd4b9490f1406c90cfb
f20008012 <clem.fresnel@gmail.com>
2022-03-02 10:08:34 +0100

    connexion et inscription

--- src/main/java/app/foodapp/model/AskUserTest.java
+++ src/main/java/app/foodapp/model/AskUserTest.java
@@ -2,15 +2,16 @@ package app.foodapp.model;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AskUserTest {
-    public static void AskUser() throws IOException, ParseException {
+    public static void AskUser(ArrayList user) throws IOException, ParseException {
         Scanner inputUser = new Scanner(System.in);
-        System.out.println("Put 1 for a query search / put 2 for an ingredient search / Put 3 to show your favorites");
+        System.out.println("Put 1 for a query search / put 2 for an ingredient search / Put 3 to show your favorites/ put 4 to Stop");
         int choice = inputUser.nextInt();
 
-        while (choice != 1 && choice!=2 && choice!=3){
+        while (choice != 1 && choice!=2 && choice!=3 && choice!=4){
             System.out.println("Put 1 for a query search / put 2 for an ingredient search / Put 3 to show your favorites");
             choice = inputUser.nextInt();
         }
@@ -18,22 +19,23 @@ public class AskUserTest {
             System.out.println("Please, give us your key word !");
             String keyWord = inputUser.next();
             if (RequestAPI.SearchByKey(keyWord)){
-               recipeInfoByID(inputUser);
+               recipeInfoByID(user,inputUser);
            }
+            AskUser(user);
         }
 
         else if (choice == 2){
             System.out.println("Please, give us your ingredient !");
             String ingredient = inputUser.next();
             if (RequestAPI.SearchByIngredient(ingredient)) {
-                recipeInfoByID(inputUser);
+                recipeInfoByID(user,inputUser);
             }
+            AskUser(user);
+
 
         }
-         else{
-            System.out.println("What is you code ?");
-            String code = inputUser.next();
-           Object favoritesDetails[]= (Object[]) FavoriteRecipes.ShowFavorites(code);
+         else if (choice == 3) {
+           Object favoritesDetails[]= (Object[]) FavoriteRecipes.ShowFavorites(user);
            Boolean favoritesIsNotEmpty= (Boolean) favoritesDetails[1];
            int numberOfFavorites= (int) favoritesDetails[0];
             if (favoritesIsNotEmpty){
@@ -43,7 +45,7 @@ public class AskUserTest {
                    System.out.println("Do you want yo delete something ? 1 for no / 2 for no");
                    number = inputUser.nextInt();
                }
-               if (number==2) AskUser();
+               if (number==2) AskUser(user);
                else
                {
                    System.out.println("Choose a number from your favorites recipes ");
@@ -53,16 +55,17 @@ public class AskUserTest {
                        System.out.println("Choose a number from your favorites recipes ");
                        numberFav = inputUser.nextInt();
                    }
-                   FavoriteRecipes.DeleteFavorite(code,numberFav);
+                   FavoriteRecipes.DeleteFavorite(user,numberFav);
                }
 
 
            }
+            AskUser(user);
 
         }
     }
 
-    private static void recipeInfoByID(Scanner inputUser) throws IOException, ParseException {
+    private static void recipeInfoByID(ArrayList<String> user ,Scanner inputUser) throws IOException, ParseException {
         System.out.println("Do you want to know more about one this recipes? just give us the number !");
         System.out.print("Number : ");
         int index = inputUser.nextInt();
@@ -75,24 +78,49 @@ public class AskUserTest {
             answ = inputUser.nextInt();
         }
         if (answ==1){
-           System.out.print("Donez votre Code ! : ");
-           String code = inputUser.next();
-            FavoriteRecipes.FillFile(recipe.getTitle(),code);
+            FavoriteRecipes.FillFile(recipe.getTitle(),user);
         }
 
 
     }
 
-    public static void main(String[] args) throws IOException, ParseException {
-        AskUser();
+    private static ArrayList<String> LogAndSign( ) throws IOException, ParseException {
+        System.out.println("Choose an option :");
+        System.out.println("1-Log in");
+        System.out.println("2-Sign in");
         Scanner inputUser = new Scanner(System.in);
-        System.out.println("Do you want to search again ? 1 for yes / 2 for no");
-        int answer = inputUser.nextInt();
-        while (answer == 1){
-            AskUser();
-            System.out.println("Do you want to search again ? 1 for yes / 2 for no");
-            answer = inputUser.nextInt();
+        int logAndSign = inputUser.nextInt();
+        System.out.print("Username : ");
+        String userName = inputUser.next();
+        System.out.print("Password : ");
+        String password = inputUser.next();
+        ArrayList<String> user = new ArrayList<>();
+        user.add(userName);
+        user.add(password);
+        user.add("yes");
+        if (logAndSign==2){
+            FavoriteRecipes.SignIn(user,logAndSign);
         }
+        else if (logAndSign==1)
+        {
+            ArrayList userUpdate =FavoriteRecipes.SignIn(user,logAndSign);
+            if (userUpdate.get(2)=="no"){
+                System.out.println("Account doesnt exist ! PLease verify spelling or create a new one !");
+                LogAndSign();
+            }
+        }
+        return user;
+
+
+
+
+
+    }
+
+
+        public static void main(String[] args) throws IOException, ParseException {
+        ArrayList user = LogAndSign();
+        AskUser(user);
     }
 
 }
--- src/main/java/app/foodapp/model/FavoriteRecipes.java
+++ src/main/java/app/foodapp/model/FavoriteRecipes.java
@@ -11,7 +11,8 @@ import java.util.ArrayList;
 public class FavoriteRecipes {
 
 
-    public static void FillFile(String FavoriteRecipe,String code) throws IOException, ParseException {
+    public static void FillFile(String FavoriteRecipe,ArrayList<String> listOfInfos) throws IOException, ParseException {
+        String code = listOfInfos.get(0)+listOfInfos.get(1);
         String fileName = "Favorites.json";
         File f = new File(fileName);
         f.createNewFile();
@@ -48,8 +49,8 @@ public class FavoriteRecipes {
 
     static ArrayList<String> nameList= new ArrayList<>();
 
-    public static Object ShowFavorites(String code) throws IOException, ParseException {
-
+    public static Object ShowFavorites(ArrayList<String> listOfInfos) throws IOException, ParseException {
+      String code = listOfInfos.get(0)+listOfInfos.get(1);
     String fileName = "Favorites.json";
     boolean containsValue= false;
     int j=-1;
@@ -61,7 +62,7 @@ public class FavoriteRecipes {
             JSONArray arrayOfRecipes = (JSONArray) jsonP.parse(new FileReader(fileName));
                 for (int i=0; i<arrayOfRecipes.size();i++) {
                     JSONObject favoriteRecipe = (JSONObject) arrayOfRecipes.get(i);
-                    if (favoriteRecipe.get(code)!=null){
+                    if (favoriteRecipe.get(code)!=null && favoriteRecipe.get(code)!=""){
                         containsValue=true;
                         nameList.add((String) favoriteRecipe.get(code));
                         j++;
@@ -81,9 +82,70 @@ public class FavoriteRecipes {
     }
 
 
-    public static void DeleteFavorite(String code, int number) throws IOException, ParseException {
+
+
+
+    public static ArrayList<String> SignIn(ArrayList<String> user, int logAndSign) throws IOException, ParseException {
         String fileName = "Favorites.json";
         File f = new File(fileName);
+        f.createNewFile();
+        if (f.isFile()) {
+            JSONParser jsonP = new JSONParser();
+            JSONArray jsonArray = new JSONArray();
+            try {
+                if (f.length()!=0){
+                    jsonArray = (JSONArray) jsonP.parse(new FileReader(fileName));
+                    JSONObject jsonObject1 = new JSONObject();
+                    jsonObject1.put(user.get(0)+user.get(1), "");
+                    if(jsonArray.contains(jsonObject1)){
+                        if (logAndSign == 2){
+                            System.out.println("Account Already created !");
+                            System.out.println("Welcome Again "+ user.get(0));
+                        }
+                        else
+                        {
+                            System.out.println("Welcome Again "+ user.get(0));
+                        }
+                        return user;
+                    }
+                    else
+                    if (logAndSign==1)
+                    {
+                        user.add(2,"no");
+                        return user;
+                    }
+                   // jsonArray.add(jsonObject1);
+                }
+                else
+                {
+                    JSONObject jsonObject1 = new JSONObject();
+                    jsonObject1.put(user, "");
+                    jsonArray.add(jsonObject1);
+                }
+                FileWriter fw = new FileWriter(fileName);
+                BufferedWriter bw = new BufferedWriter(fw);
+                fw.write(String.valueOf(jsonArray));
+                bw.newLine();
+                fw.close();
+                System.out.println("Account created !");
+                System.out.println("Welcome  "+ user.get(0));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return user;
+    }
+
+
+
+
+
+
+        public static void DeleteFavorite(ArrayList<String> listOfInfos, int number) throws IOException, ParseException {
+        String fileName = "Favorites.json";
+        String code = listOfInfos.get(0)+listOfInfos.get(1);
+            File f = new File(fileName);
         f.createNewFile();
         if (f.isFile()) {
             if (f.length()!=0){
